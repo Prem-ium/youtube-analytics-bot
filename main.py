d2f7b8f26f2a00704d688477e365e008cbb2243d
Prem-ium <thefrostscoper@gmail.com>
2022-12-01 19:21:39 -0500

    Fixing default dates, changing command names, etc.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import os
-import datetime
-import traceback
+import os, datetime, traceback, calendar
+
+from calendar import monthrange
 from dotenv import load_dotenv
 from time import sleep
-import discord
-from discord.ext import commands, tasks
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-from googleapiclient.discovery import build
-import googleapiclient.errors
-from google_auth_oauthlib.flow import InstalledAppFlow
+
 import google
 import google_auth_oauthlib
-import calendar
+import googleapiclient.errors
+import discord
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from oauth2client.file import Storage
+from discord.ext import commands, tasks
+from oauth2client import client, tools
 
 load_dotenv()
 
@@ -22,11 +22,16 @@ SCOPES = ["https://www.googleapis.com/auth/youtube.readonly",
 
 if not os.environ["DISCORD_TOKEN"]:
     raise Exception("DISCORD_TOKEN is missing within .env file, please add it and try again.")
+elif not os.environ["YOUTUBE_API_KEY"]:
+    raise Exception("This bot relies on YouTube Analytics AND Data API, please enable YouTube Data API.\nInsert the YouTube Data API key within YOUTUBE_API_KEY varibale in .env and try again.\n")
+
 DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 DISCORD_CHANNEL = os.environ.get("DISCORD_CHANNEL", None)
 
 if DISCORD_CHANNEL:
     DISCORD_CHANNEL = int(DISCORD_CHANNEL)
+
+YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
 
 # Whether to use keep_alive.py
 if (os.environ.get("KEEP_ALIVE", "False").lower() == "true"):
@@ -48,16 +53,39 @@ def execute_api_request(client_library_function, **kwargs):
     return client_library_function(**kwargs).execute()
 
 async def update_dates(startDate, endDate):
-    if len(startDate) != 5 or len(endDate) != 5:
-        startDate = datetime.datetime.strptime(startDate, '%m/%d/%y').strftime('%Y/%m/%d').replace('/', '-')
-        endDate = datetime.datetime.strptime(endDate, '%m/%d/%y').strftime('%Y/%m/%d').replace('/', '-')
+    splitStartDate = startDate.split('/')
+    splitEndDate = endDate.split('/')
+
+    if (splitStartDate[0] == 1 and splitEndDate[0] == 1) or startDate == endDate:
+        year = startDate.split('/')[2] if (len(startDate.split('/')) > 2) else datetime.datetime.now().strftime("%Y")
+        year = f'20{year}' if len(year) == 2 else year
+        previousMonth = int(splitStartDate[0]) - \
+            1 if int(splitStartDate[0]) > 1 else 12
+        lastDay = monthrange(int(year), previousMonth)[1]
+
+        # Assign the new dates to the variables & return them
+        startDate = datetime.datetime.strptime(
+            f'{previousMonth}/01', '%m/%d').strftime(f'{year}/%m/%d').replace('/', '-')
+        endDate = datetime.datetime.strptime(
+            f'{previousMonth}/{lastDay}', '%m/%d').strftime(f'{year}/%m/%d').replace('/', '-')
+
+        return startDate, endDate
+    elif len(startDate) != 5 or len(endDate) != 5:
+        startDate = datetime.datetime.strptime(
+            startDate, '%m/%d/%y').strftime('%Y/%m/%d').replace('/', '-')
+        endDate = datetime.datetime.strptime(
+            endDate, '%m/%d/%y').strftime('%Y/%m/%d').replace('/', '-')
     else:
         currentYear = datetime.datetime.now().strftime("%Y")
         if len(startDate) == 5:
-            startDate = datetime.datetime.strptime(startDate, '%m/%d').strftime(f'{currentYear}/%m/%d').replace('/', '-')
+            startDate = datetime.datetime.strptime(
+                startDate, '%m/%d').strftime(f'{currentYear}/%m/%d').replace('/', '-')
         if len(endDate) == 5:
-            endDate = datetime.datetime.strptime(endDate, '%m/%d').strftime(f'{currentYear}/%m/%d').replace('/', '-')
+            endDate = datetime.datetime.strptime(
+                endDate, '%m/%d').strftime(f'{currentYear}/%m/%d').replace('/', '-')
+    print(f'Updating dates to {startDate} - {endDate}')
     return startDate, endDate
+
 
 async def get_stats(start=datetime.datetime.now().strftime("%Y-%m-01"), end=datetime.datetime.now().strftime("%Y-%m-%d")):
     try:
@@ -76,7 +104,8 @@ async def get_stats(start=datetime.datetime.now().strftime("%Y-%m-01"), end=date
         cpm = response['rows'][0][3]
 
         # Terminary operator to check if start/end year share a year, and strip/remove if that's the case
-        start, end = (start[5:] if start[:4] == end[:4] else f'{start[5:]}-{start[:4]}').replace('-', '/'), (end[5:] if start[:4] == end[:4] else f'{end[5:]}-{end[:4]}').replace('-', '/')
+        start, end = (start[5:] if start[:4] == end[:4] else f'{start[5:]}-{start[:4]}').replace(
+            '-', '/'), (end[5:] if start[:4] == end[:4] else f'{end[5:]}-{end[:4]}').replace('-', '/')
 
         response = f'YouTube Analytics Report ({start}\t-\t{end})\n\nViews:\t{round(views,2):,}\nMinutes Watched:\t{round(minutes,2):,}\nEstimated Revenue:\t${round(revenue,2):,}\nPlayback CPM:\t${round(cpm,2):,}'
         print(response + '\nSending to Discord...')
@@ -86,7 +115,7 @@ async def get_stats(start=datetime.datetime.now().strftime("%Y-%m-01"), end=date
         return f"Ran into {e.__class__.__name__} exception, please check the logs."
 
 
-async def top_revenue(results= 10, start=datetime.datetime.now().strftime("%Y-%m-01"), end=datetime.datetime.now().strftime("%Y-%m-%d")):
+async def top_revenue(results=10, start=datetime.datetime.now().strftime("%Y-%m-01"), end=datetime.datetime.now().strftime("%Y-%m-%d")):
     try:
         youtubeAnalytics = get_service()
         response = execute_api_request(
@@ -105,7 +134,7 @@ async def top_revenue(results= 10, start=datetime.datetime.now().strftime("%Y-%m
             video_ids.append(data[0])
             earnings.append(data[1])
 
-        youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=os.environ.get('YOUTUBE_API_KEY'))
+        youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
 
         request = youtube.videos().list(
             part="snippet",
@@ -123,8 +152,8 @@ async def top_revenue(results= 10, start=datetime.datetime.now().strftime("%Y-%m
     except Exception as e:
         print(traceback.format_exc())
         return f"Ran into {e.__class__.__name__} exception, please check the logs."
-    
-async def top_countries_by_revenue(results = 10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
+
+async def top_countries_by_revenue(results=10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
     try:
         youtubeAnalytics = get_service()
         # Get top preforming countries by revenue
@@ -138,7 +167,8 @@ async def top_countries_by_revenue(results = 10, startDate=datetime.datetime.now
             sort='-grossRevenue',
             maxResults=results,
         )
-        startDate, endDate = (startDate[5:] if startDate[:4] == endDate[:4] else f'{startDate[5:]}-{startDate[:4]}').replace('-', '/'), (endDate[5:] if startDate[:4] == endDate[:4] else f'{endDate[5:]}-{endDate[:4]}').replace('-', '/')
+        startDate, endDate = (startDate[5:] if startDate[:4] == endDate[:4] else f'{startDate[5:]}-{startDate[:4]}').replace(
+            '-', '/'), (endDate[5:] if startDate[:4] == endDate[:4] else f'{endDate[5:]}-{endDate[:4]}').replace('-', '/')
 
         returnString = f'Top {results} Countries by Revenue: ({startDate}\t-\t{endDate})\n'
         for row in response['rows']:
@@ -164,7 +194,8 @@ async def get_ad_preformance(start=datetime.datetime.now().strftime("%Y-%m-01"),
             sort='adType'
         )
         # Terminary operator to check if start/end year share a year, and strip/remove if that's the case
-        start, end = (start[5:] if start[:4] == end[:4] else f'{start[5:]}-{start[:4]}').replace('-', '/'), (end[5:] if start[:4] == end[:4] else f'{end[5:]}-{end[:4]}').replace('-', '/')
+        start, end = (start[5:] if start[:4] == end[:4] else f'{start[5:]}-{start[:4]}').replace(
+            '-', '/'), (end[5:] if start[:4] == end[:4] else f'{end[5:]}-{end[:4]}').replace('-', '/')
 
         preformance = f'Ad Preformance ({start}\t-\t{end})\n\n'
         # Parse the response into nice formatted string
@@ -175,7 +206,6 @@ async def get_ad_preformance(start=datetime.datetime.now().strftime("%Y-%m-01"),
     except Exception as e:
         print(traceback.format_exc())
         return f"Ran into {e.__class__.__name__} exception, please check the logs."
-
 
 if __name__ == "__main__":
     intents = discord.Intents.all()
@@ -192,72 +222,93 @@ if __name__ == "__main__":
     # Bot ping-pong
     @bot.command(name='ping')
     async def ping(ctx):
-        print('Someone just got ponged!')
         await ctx.send('pong')
+        print(
+            f'\n{ctx.author.name} just got ponged!\t{datetime.datetime.now().strftime("%m/%d %H:%M:%S")}\n')
 
-    @bot.command(aliases=['lifetime', 'alltime'])
-    async def lifetime_method(ctx):
-        print()
-        # Get Lifetime stats from the get_stats function, and send it to the channel
-        await ctx.send(await get_stats('2005-02-14', datetime.datetime.now().strftime("%Y-%m-%d")))
-        print('\nLifetime stats sent\n')
-
-    @bot.command(aliases=['lastMonth'])
-    async def lastmonth(ctx):
-        startDate = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
-        endDate = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
-        startDate = startDate.replace(day=1)
-        endDate = endDate.replace(day=calendar.monthrange(endDate.year, endDate.month)[1])
-        # Get last months stats from the get_stats function, and send it to the channel
-        await ctx.send(await get_stats(startDate.strftime("%Y-%m-%d"), endDate.strftime("%Y-%m-%d")))
-        print('\nLast months stats sent\n')
-
-    @bot.command(aliases=['stats'])
+    # Retrieve Analytic stats within specified date range, defaults to current month
+    @bot.command(aliases=['stats', 'thisMonth'])
     async def analyze(ctx, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
         startDate, endDate = await update_dates(startDate, endDate)
         await ctx.send(await get_stats(startDate, endDate))
         print(f'\n{startDate} - {endDate} stats sent')
 
+    # Retrieve top earning videos within specified date range, defaults to current month
+    @bot.command(aliases=['lastMonth'])
+    async def lastmonth(ctx):
+        startDate = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+        endDate = datetime.date.today().replace(day=1) - datetime.timedelta(days=1)
+        startDate = startDate.replace(day=1)
+        endDate = endDate.replace(
+            day=calendar.monthrange(endDate.year, endDate.month)[1])
+        await ctx.send(await get_stats(startDate.strftime("%Y-%m-%d"), endDate.strftime("%Y-%m-%d")))
+        print(f'\nLast month ({startDate} - {endDate}) stats sent\n')
+
+    # Retrieve top earning videos within specified date range between any month/year, defaults to current month
+    @bot.command(aliases=['getMonth'])
+    async def month(ctx, period=datetime.datetime.now().strftime("%m/%Y")):
+        period = period.split('/')
+        month, year = period[0], period[1]
+        year = f'20{year}' if len(year) == 2 else year
+        lastDate = monthrange(int(year), int(month))[1]
+        startDate = datetime.datetime.strptime(
+            f'{month}/01', '%m/%d').strftime(f'{year}/%m/%d').replace('/', '-')
+        endDate = datetime.datetime.strptime(
+            f'{month}/{lastDate}', '%m/%d').strftime(f'{year}/%m/%d').replace('/', '-')
+        await ctx.send(await get_stats(startDate, endDate))
+        print(f'\n{period} stats sent\n')
+
+    # Retrieve top earning videos within specified date range, defaults to current month
     @bot.command(aliases=['topEarnings'])
-    async def top(ctx, results = 10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
+    async def top(ctx, results=10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
         startDate, endDate = await update_dates(startDate, endDate)
         await ctx.send(await top_revenue(results, startDate, endDate))
         print(f'\n{startDate} - {endDate} top {results} sent')
 
-    @bot.command(aliases=['everything'])
-    async def all(ctx, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
+    # Top revenue by country
+    @bot.command(aliases=['geo_revenue', 'geoRevenue'])
+    async def country(ctx, results=10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
         startDate, endDate = await update_dates(startDate, endDate)
-        await ctx.send(await get_stats(startDate, endDate) + '\n\n.')
-        await ctx.send(await top_revenue(10, startDate, endDate)+ '\n\n.')
-        await ctx.send(await top_countries_by_revenue(10, startDate, endDate)+ '\n\n.')
-        await ctx.send(await get_ad_preformance(startDate, endDate)+ '\n\n.')
-
-        print(f'\n{startDate} - {endDate} everything sent')
-
-    @bot.command(aliases=['earningsByCountry'])
-    async def country(ctx, results = 10, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
-        startDate, endDate = await update_dates(startDate, endDate)
-        await ctx.send(await top_countries_by_revenue(results,startDate, endDate))
+        await ctx.send(await top_countries_by_revenue(results, startDate, endDate))
         print(f'\n{startDate} - {endDate} earnings by country sent')
 
-    @bot.command(aliases=['adPreformance'])
+    # Ad Type Preformance Data
+    @bot.command(aliases=['adtype', 'adPreformance'])
     async def ad(ctx, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
         startDate, endDate = await update_dates(startDate, endDate)
         await ctx.send(await get_ad_preformance(startDate, endDate))
         print(f'\n{startDate} - {endDate} ad preformance sent')
 
+    # Lifetime stats
+    @bot.command(aliases=['lifetime', 'alltime'])
+    async def lifetime_method(ctx):
+        await ctx.send(await get_stats('2005-02-14', datetime.datetime.now().strftime("%Y-%m-%d")))
+        print('\nLifetime stats sent\n')
+
+    # Send everything.
+    @bot.command(aliases=['everything'])
+    async def all(ctx, startDate=datetime.datetime.now().strftime("%m/01/%y"), endDate=datetime.datetime.now().strftime("%m/%d/%y")):
+        startDate, endDate = await update_dates(startDate, endDate)
+        await ctx.send(await get_stats(startDate, endDate) + '\n\n.')
+        await ctx.send(await top_revenue(10, startDate, endDate) + '\n\n.')
+        await ctx.send(await top_countries_by_revenue(10, startDate, endDate) + '\n\n.')
+        await ctx.send(await get_ad_preformance(startDate, endDate) + '\n\n.')
+
+        print(f'\n{startDate} - {endDate} everything sent')
     # Help command
+
     @bot.command()
     async def help(ctx):
         await ctx.send('Available commands:')
-        await ctx.send('!everything - Return everything. Call every method and output all available data')
-        await ctx.send('!stats [startDate] [endDate]- Return stats within time range. Defaults to current month')
+        await ctx.send('!stats [startDate] [endDate]- Return stats within time range. Defaults to current month\nExample: !stats 01/01 12/1 \t!stats 01/01/2021 01/31/2021')
+        await ctx.send('!getMonth [month/year]- Return stats for a specific month.\nExample: !getMonth 01/21')
         await ctx.send('!topEarnings [# of countries to return] [startDate] [endDate]- Return top specified highest revenue earning videos.')
-        await ctx.send('!earningsByCountry [# of countries to return] [startDate] [endDate] - Top Specific (default 10) countries by revenue')
-        await ctx.send('!adPreformance [startDate] [endDate] - Get ad preformance for a given date range')
+        await ctx.send('!geo_revenue [# of countries to return] [startDate] [endDate] - Top Specific (default 10) countries by revenue')
+        await ctx.send('!adtype [startDate] [endDate] - Get highest preforming ad types within specified time range')
         await ctx.send('!lifetime - Get lifetime stats')
-        await ctx.send('!restart')
-        await ctx.send('!help')
+        await ctx.send('!everything [startDate] [endDate]- Return everything. Call every method and output all available data')
+        await ctx.send('!restart - Restart the bot')
+        await ctx.send('!help\n!ping')
 
     # Restart command
     @bot.command(name='restart')
