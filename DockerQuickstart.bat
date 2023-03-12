973aec8b9031b46417050beb8c1b90823445d663
Prem-ium <thefrostscoper@gmail.com>
2023-03-12 09:12:25 -0400

    Improving Readability

--- DockerQuickstart.bat
+++ DockerQuickstart.bat
@@ -1,8 +1,11 @@
 docker stop youtube-apprise
 docker rm youtube-apprise
+
 REM Replace the path below with the path to your youtube-analytics-bot folder
 cd Desktop\youtube-analytics-bot
+
 docker build -t youtube-apprise .
 docker run -itd --env-file ./.env --restart unless-stopped --name youtube-apprise youtube-apprise
-REM WARNING: This will remove all dangling images. If you have other images that you want to keep, do not run this command. Otherwise, run this command to clean up your old docker images.
+
+REM WARNING: This will remove all dangling images. Uncomment the line below if you want to do this.
 REM docker image prune -f --filter "dangling=true"
