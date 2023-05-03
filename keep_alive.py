0b4b0955a308b368b3ee48494b91fd7c66748986
Prem-ium <thefrostscoper@gmail.com>
2023-05-03 18:42:44 -0400

    Comment Instructions for Keep Alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,12 @@
+# Full Instructions:
+# https://repl.it/talk/learn/Hosting-discordpy-bots-with-replit/11008
+# Run this file to keep the bot alive on repl.it
+
+# Enable Keep Alive by using the KEEP_ALIVE variable in .env
+# After running, you can copy the URL from the console and paste it into an UptimeRobot
+# monitor to keep the bot alive 24/7
+
+
 from flask import Flask
 from threading import Thread
 
