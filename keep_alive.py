0f4727e0cbc1bf0f879c33a918ffbe8b9ea4911a
Prem-ium <thefrostscoper@gmail.com>
2024-02-17 18:37:31 -0500

    Remove Repl.it Instructions

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,3 @@
-# Full Instructions:
-# https://repl.it/talk/learn/Hosting-discordpy-bots-with-replit/11008
-# Run this file to keep the bot alive on repl.it
-
-# Enable Keep Alive by using the KEEP_ALIVE variable in .env
-# After running, you can copy the URL from the console and paste it into an UptimeRobot
-# monitor to keep the bot alive 24/7
-
-
 from flask import Flask
 from threading import Thread
 
@@ -14,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alivvveee! All thanks to UpTimeRobot! :D"
+    return "I'm alivvveee! This is a Discord bot made by @Prem-ium on GitHub."
 
 def run():
     app.run(host = '0.0.0.0', port = 8080)
